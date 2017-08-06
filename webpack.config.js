523b760da26003a05d3595e60f2981981370366a
M-ulyanov <web.ulyanov@gmail.com>
2017-08-06 20:21:48 +0300

    Add dist

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: { path: __dirname, filename: 'dist/bundle.js' },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react', 'stage-1']
+        }
+      }
+    ]
+  },
+};
